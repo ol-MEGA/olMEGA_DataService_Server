fb9899a151c31398a1345dcd39e7a83948f46935
Sven Franz <sven.franz@jade-hs.de>
2021-12-23 16:55:57 +0100

    DevelServer Boolean added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from olMEGA_DataService_Server import olMEGA_DataService_Server as app
 
 # https://blog.miguelgrinberg.com/post/running-your-flask-application-over-https
+develServer = True
 
 if __name__ == "__main__":
     Server = app.olMEGA_DataService_Server()
