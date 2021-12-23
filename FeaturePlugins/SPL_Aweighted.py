e90c39426a05fe7fa10ec9ca2633db7cb775e453
Joerg Bitzer <joerg.bitzer@jade-hs.de>
2021-12-23 18:31:26 +0100

    now nan solved

--- FeaturePlugins/SPL_Aweighted.py
+++ FeaturePlugins/SPL_Aweighted.py
@@ -1,9 +1,14 @@
 """
-PLugIn to compute the a weighted RMS of OlMEGA Data
+PLugIn to compute the a weighted RMS of OlMEGA Data. The results are the mean (linear mean of all values) in dB and
+the standard deviation of the dB values für each 125ms block.
 """
 
 # (c) Joerg Bitzer @ Jade HS, BSD 3-clause license is the valid license for this source code
 # version 0.9 mean is working (std is missing, wait for different interface)
+# version 0.9.5 changed interface to multi feature
+# version 1.0.0 rc-1 0.0 replaced by means of data
+# version 1.0.0 tested and worked
+
 
 import datetime
 import numpy as np
@@ -62,9 +67,12 @@ class SPL_A_Mean_60:
             
             while sliceStart < endTime:
                 data_block = rms_psd[counter*frames_per_block:(counter+1)*frames_per_block]
+                mean_data_block = np.mean(data_block)
+                # data extension for non possible values
+                data_block[data_block == 0] = mean_data_block
                 log_data_block = 10*np.log10(data_block)
                 cur_std = np.nanstd(log_data_block,)
-                cur_rms = 10*np.log10(np.mean(data_block))
+                cur_rms = 10*np.log10(mean_data_block)
                 counter += 1
                 sliceEnd = sliceStart + datetime.timedelta(seconds= min(60, self.timedelta))
                 values_mean.append({"start": sliceStart, "end": sliceEnd, "side": "B", "value": cur_rms, "isvalid": 1})
