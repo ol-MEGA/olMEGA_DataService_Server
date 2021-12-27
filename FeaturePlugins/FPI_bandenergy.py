70819593c59c67f64b76630b0bdcc1b0bd4c399c
Joerg Bitzer <joerg.bitzer@jade-hs.de>
2021-12-27 20:00:13 +0100

    added Bandenerg

--- FeaturePlugins/FPI_bandenergy.py
+++ FeaturePlugins/FPI_bandenergy.py
@@ -1,29 +1,27 @@
 """
-PLugIn to compute the a weighted RMS of OlMEGA Data. The results are the mean (linear mean of all values) in dB and
-the standard deviation of the dB values für each 125ms block.
+Feature-PLugIn (FPI) to compute the mean of the bandenergy for 125ms blocks in the octave bands from 125 Hz upt o 4kHz 
 """
 
 # (c) Joerg Bitzer @ Jade HS, BSD 3-clause license is the valid license for this source code
-# version 0.9 mean is working (std is missing, wait for different interface)
-# version 0.9.5 changed interface to multi feature
-# version 1.0.0 rc-1 0.0 replaced by means of data
-# version 1.0.0 tested and worked
+# version 0.1 init
 
 
 import datetime
 import numpy as np
-import FeaturePlugins.acousticweighting as aw
+import FeaturePlugins.freq2freqtransforms as ft
 
 import matplotlib.pyplot as plt
 
 class SPL_A_Mean_60:
-    feature = ['SPL(A)_broadband_mean_10s', 'SPL(A)_broadband_stddev_10s']
-    description = ['The mean (mu) of 10s A-weighted SPL value (computed by all 125ms SPL blocks)', 'The Standard Deviation (sigma) of 10s A-weighted SPL value (Attention in dB) (computed by all 125ms SPL blocks)']
+    feature = ['band_energy_125Hz','band_energy_250Hz','band_energy_500Hz','band_energy_1000Hz','band_energy_2000Hz','band_energy_4000Hz']
+    description = ['The mean (mu) of the 125Hz band of 60s data (125ms blocks)','The mean (mu) of the 250Hz band of 60s data (125ms blocks)',
+                   'The mean (mu) of the 500Hz band of 60s data (125ms blocks)','The mean (mu) of the 1000Hz band of 60s data (125ms blocks)',
+                   'The mean (mu) of the 2000Hz band of 60s data (125ms blocks)','The mean (mu) of the 4000Hz band of 60s data (125ms blocks)']
     isActive = True
     storeAsFeatureFile = False # False: single Value stored in Database, True: Matrix stored in FeatureFile
     
     def __init__(self):
-        self.timedelta = 10 # in seconds (max: 60)
+        self.timedelta = 60 # in seconds (max: 60)
         if (self.timedelta >= 60):
             self.timedelta = 60
         
@@ -42,10 +40,13 @@ class SPL_A_Mean_60:
             Pxx = PSD['Pxx']
             Pyy = PSD['Pyy']
             nr_of_frames, fft_size = Pxx.shape
-            w,f = aw.get_fftweight_vector((fft_size-1)*2,fs,'a','lin')
-            meanPSD = (((Pxx+Pyy)*0.5*fs)*w)*0.25 # this works because of broadcasting rules in python
-            rms_psd = np.mean((meanPSD), axis=1) # mean over frequency
-            
+            weight_mat,a,b = ft.get_spectrum_fractionaloctave_transformmatrix((fft_size-1)*2,fs,125,4000,1)
+            mu_Pxx = np.mean(Pxx, axis = 0)
+            mu_Pyy = np.mean(Pyy, axis = 0)
+            band_energL = (mu_Pxx@weight_mat)
+            band_energR = (mu_Pyy@weight_mat)
+            band_energ = 10*np.log10(0.5*(band_energL +band_energR))
+                        
             # just debug
             #RMS = existingFeatures['RMS']
             #print(10*np.log10(np.mean(rms_psd)))
@@ -58,25 +59,23 @@ class SPL_A_Mean_60:
             #plt.show()
             
             
-            values_mean = []
-            values_std = []
+            values_125 = []
+            values_250 = []
+            values_500 = []
+            values_1000 = []
+            values_2000 = []
+            values_4000 = []
             
             sliceStart = startTime
-            counter = int(0)
-            frames_per_block = int(nr_of_frames/self.nr_of_blocks)
             
             while sliceStart < endTime:
-                data_block = rms_psd[counter*frames_per_block:(counter+1)*frames_per_block]
-                mean_data_block = np.mean(data_block)
-                # data extension for non possible values
-                data_block[data_block == 0] = mean_data_block
-                log_data_block = 10*np.log10(data_block)
-                cur_std = np.nanstd(log_data_block,)
-                cur_rms = 10*np.log10(mean_data_block)
-                counter += 1
                 sliceEnd = sliceStart + datetime.timedelta(seconds= min(60, self.timedelta))
-                values_mean.append({"start": sliceStart, "end": sliceEnd, "side": "B", "value": cur_rms, "isvalid": 1})
-                values_std.append({"start": sliceStart, "end": sliceEnd, "side": "B", "value": cur_std, "isvalid": 1})
+                values_125.append({"start": sliceStart, "end": sliceEnd, "side": "B", "value": band_energ[0], "isvalid": 1})
+                values_250.append({"start": sliceStart, "end": sliceEnd, "side": "B", "value": band_energ[1], "isvalid": 1})
+                values_500.append({"start": sliceStart, "end": sliceEnd, "side": "B", "value": band_energ[2], "isvalid": 1})
+                values_1000.append({"start": sliceStart, "end": sliceEnd, "side": "B", "value": band_energ[3], "isvalid": 1})
+                values_2000.append({"start": sliceStart, "end": sliceEnd, "side": "B", "value": band_energ[4], "isvalid": 1})
+                values_4000.append({"start": sliceStart, "end": sliceEnd, "side": "B", "value": band_energ[5], "isvalid": 1})
                 sliceStart = sliceEnd
-            return values_mean, values_std
+            return values_125,values_250,values_500,values_1000,values_2000,values_4000
 
